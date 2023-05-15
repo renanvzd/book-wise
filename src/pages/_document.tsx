31639283d012202ae46ab64ee1b353ef7efa8be4
Renan Veronez Drechsler <renanvz@outlook.com>
2023-05-15 14:43:44 +0100

    Reset project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { getCssText } from "../../stitches.config";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       <body>
         <Main />
         <NextScript />
