92c43464782a862bd4bcc44d323018a11e140f76
Renan Veronez Drechsler <renanvz@outlook.com>
2023-05-16 14:14:13 +0100

    Book details

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -2,6 +2,7 @@ import { Book } from "@prisma/client"
 import { Text } from "../Typography"
 import { BookDetails, BookImage, BookName, Container, ReadBadge } from "./styles"
 import { RatingStars } from "../RatingStars"
+import { RatingsDialog } from "../RatingsDialog"
 
 export type BookWithAvgRating = Book & {
   avgRating: number
@@ -28,20 +29,22 @@ export const BookCard = ({ book, size = "md" }: BookCardProps) => {
   const currentSize = IMAGE_SIZES[size]
 
   return (
-    <Container>
-      <BookImage width={currentSize.width} height={currentSize.height} css={{ minWidth: currentSize.width }} alt={book.name} src={book.cover_url} />
-      <BookDetails>
-        <div>
-          <BookName size="xs">
-            {book.name}
-          </BookName>
-          <Text size="sm" color="gray-400">
-            {book.author}
-          </Text>
-        </div>
+    <RatingsDialog bookId={book?.id}>
+      <Container>
+        <BookImage width={currentSize.width} height={currentSize.height} css={{ minWidth: currentSize.width }} alt={book.name} src={book.cover_url} />
+        <BookDetails>
+          <div>
+            <BookName size="xs">
+              {book.name}
+            </BookName>
+            <Text size="sm" color="gray-400">
+              {book.author}
+            </Text>
+          </div>
 
-        <RatingStars rating={book.avgRating} />
-      </BookDetails>
-    </Container>
+          <RatingStars rating={book.avgRating} />
+        </BookDetails>
+      </Container>
+    </RatingsDialog>
   )
 }
